96ef6b1dfed98419f02b024e9208ff16b09d2eef
tvasuper <68137088+tvasuper@users.noreply.github.com>
2024-07-09 21:36:31 +0900

    Delete cto.bat

--- cto.bat
+++
@@ -1,4 +0,0 @@
-rmdir /s /q "%appdata%\.minecraft\saves\P_K 乞青技拌"
-xcopy world "%appdata%\.minecraft\saves\P_K 乞青技拌\" /e /y
-xcopy "world_nether\DIM-1" "%appdata%\.minecraft\saves\P_K 乞青技拌\DIM-1\" /e /y
-xcopy "world_the_end\DIM1" "%appdata%\.minecraft\saves\P_K 乞青技拌\DIM1\" /e /y
